e6e6f35baa2cf39d702e48bc51f8b4d55d7c4d45
Benjamin David <ben@dukt.net>
2016-06-02 15:29:16 +0200

    Added package.json and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var version = '0.1';
+
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	insert = require('gulp-insert'),
+	uglify = require('gulp-uglify');
+
+var srcDir = './Source/';
+var outDir = './dist/';
+
+var header = "/**\n" +
+	" * Garnish UI toolkit\n" +
+	" *\n" +
+	" * @copyright 2013 Pixel & Tonic, Inc.. All rights reserved.\n" +
+	" * @author    Brandon Kelly <brandon@pixelandtonic.com>\n" +
+	" * @version   " + version + "\n" +
+	" * @license   THIS IS NO F.O.S.S!\n" +
+	" */\n" +
+	"(function($){\n" +
+	"\n";
+
+var footer = "\n" +
+    "})(jQuery);\n";
+
+gulp.task('build', buildTask);
+gulp.task('watch', watchTask);
+
+gulp.task('default', ['build', 'watch']);
+
+function buildTask()
+{
+	return gulp.src([
+			srcDir+'/*.js',
+			srcDir+'/classes/*.js',
+		])
+		.pipe(concat('garnish-'+version+'.js'))
+		.pipe(insert.prepend(header))
+		.pipe(insert.append(footer))
+		.pipe(gulp.dest(outDir))
+		.pipe(uglify())
+		.pipe(concat('garnish-'+version+'.min.js'))
+		.pipe(gulp.dest(outDir));
+
+}
+
+function watchTask()
+{
+	console.log('watch task');
+}
