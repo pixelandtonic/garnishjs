becb9ebd0701913432c2a48d862620e9d633ea6e
Benjamin David <ben@dukt.net>
2016-06-30 10:30:35 +0200

    Improved Gulp error handling with `gulp-notify` and `gulp-plumber`

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,14 @@ var gulp = require('gulp'),
 	insert = require('gulp-insert'),
 	uglify = require('gulp-uglify'),
 	watch = require('gulp-watch'),
-	sourcemaps = require('gulp-sourcemaps');
+	sourcemaps = require('gulp-sourcemaps'),
+	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber');
 
 var srcDir = './src/';
 var outDir = './dist/';
 
-var header = "/**\n" +
+var jsHeader = "/**\n" +
 	" * Garnish UI toolkit\n" +
 	" *\n" +
 	" * @copyright 2013 Pixel & Tonic, Inc.. All rights reserved.\n" +
@@ -21,8 +23,22 @@ var header = "/**\n" +
 	"(function($){\n" +
 	"\n";
 
-var footer = "\n" +
+var jsFooter = "\n" +
     "})(jQuery);\n";
+
+//error notification settings for plumber
+var plumberErrorHandler = function(err) {
+
+	notify.onError({
+		title: "Garnish",
+		message:  "Error: <%= error.message %>",
+		sound:    "Beep"
+	})(err);
+
+	console.log( 'plumber error!' );
+
+	this.emit('end');
+};
 
 gulp.task('build', buildTask);
 gulp.task('watch', watchTask);
@@ -35,10 +51,11 @@ function buildTask()
 			srcDir+'*.js',
 			srcDir+'classes/*.js',
 		])
+		.pipe(plumber({ errorHandler: plumberErrorHandler }))
 		.pipe(sourcemaps.init())
 		.pipe(concat('garnish-'+version+'.js'))
-		.pipe(insert.prepend(header))
-		.pipe(insert.append(footer))
+		.pipe(insert.prepend(jsHeader))
+		.pipe(insert.append(jsFooter))
 		.pipe(gulp.dest(outDir))
 		.pipe(uglify())
 		.pipe(concat('garnish-'+version+'.min.js'))
