ac2d42e041fe74c4a2808d6e91e46f05e07db860
Benjamin David <ben@dukt.net>
2016-08-25 00:29:37 +0200

    Testing that the Menu shows up properly

--- test/MenuTest.js
+++ test/MenuTest.js
@@ -1,13 +1,19 @@
 describe("Garnish.Menu tests", function() {
 
+	var $menu = $('<div class="menu tagmenu"/>').appendTo(Garnish.$bod),
+		$ul = $('<ul/>').appendTo($menu);
+
+	var menu = new Garnish.Menu($menu);
+
 	it("Should instantiate the Menu.", function() {
-
-		var $menu = $('<div class="menu tagmenu"/>').appendTo(Garnish.$bod),
-			$ul = $('<ul/>').appendTo($menu);
-
-		var menu = new Garnish.Menu($menu);
-
 		expect(menu.menuId).toEqual('menu' + menu._namespace);
+	});
+
+	it("Should show the Menu.", function() {
+		menu.show();
+
+		expect(menu.$container.css('opacity')).toEqual('1');
+		expect(menu.$container.css('display')).toEqual('block');
 	});
 
 });
