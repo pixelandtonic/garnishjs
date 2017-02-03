a388ef821a48dcbe4e59b4a84705bc378f645ec3
August Miller <hello@gusmiller.com>
2017-02-02 18:12:37 -0800

    Fix incorrect HTML entity for nonbreaking space

--- dist/garnish.js
+++ dist/garnish.js
@@ -3,7 +3,7 @@
  *
  * @copyright 2013 Pixel & Tonic, Inc.. All rights reserved.
  * @author    Brandon Kelly <brandon@pixelandtonic.com>
- * @version   0.1.12
+ * @version   0.1.13
  * @license   MIT
  */
 (function($){
@@ -4544,7 +4544,7 @@ Garnish.NiceText = Garnish.Base.extend(
                 });
 
                 // Line breaks
-                val = val.replace(/[\n\r]$/g, '<br/>&nbps;');
+                val = val.replace(/[\n\r]$/g, '<br/>&nbsp;');
                 val = val.replace(/[\n\r]/g, '<br/>');
             }
 
